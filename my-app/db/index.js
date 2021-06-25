858a9ebc38cd755757f9816b965bd1600faf97fb
Erica <ericakrebs1@gmail.com>
2021-06-25 12:19:28 -0500

    db tables and initial data

--- my-app/db/index.js
+++ my-app/db/index.js
@@ -1,13 +1,374 @@
-// Connect to DB
 const { Client } = require("pg");
 const DB_NAME = "localhost:5432/grace-shopper-db";
 const DB_URL = process.env.DATABASE_URL || `postgres://${DB_NAME}`;
 const client = new Client(DB_URL);
 
-// database methods
+// PRODUCTS
 
-// export
+const createProduct = async ({
+  name,
+  description,
+  price,
+  image_url,
+  category,
+  quantity,
+  inventory,
+}) => {
+  try {
+    const {
+      rows: [products],
+    } = await client.query(
+      `
+            INSERT INTO products(
+              name,
+              description,
+              price,
+              image_url,
+              category,
+              quantity,
+              inventory
+              )
+            VALUES($1, $2, $3, $4, $5, $6, $7)
+            ON CONFLICT (name) DO NOTHING
+            RETURNING *;
+         `,
+      [name, description, price, image_url, category, quantity, inventory]
+    );
+    return products;
+  } catch (err) {
+    console.error("Could not create products ");
+    throw err;
+  }
+};
+
+async function getAllProducts() {
+  try {
+    const { rows } = await client.query(`
+    SELECT * FROM products;`);
+    return rows;
+  } catch (err) {
+    console.error("Could not get all products");
+    throw error;
+  }
+}
+
+async function getProductById(product_id) {
+  try {
+    const {
+      rows: [products],
+    } = await client.query(`
+        SELECT *
+        FROM products
+        WHERE id=${product_id};
+      `);
+
+    if (!products) {
+      throw {
+        name: "ProductNotFoundError",
+        message: "Could not find a product with that product_id",
+      };
+    }
+
+    return products;
+  } catch (error) {
+    console.error("Could not grab product by id ");
+    throw error;
+  }
+}
+
+async function getAllProducts() {
+  try {
+    const { rows } = await client.query(`
+    SELECT * FROM products;`);
+    return rows;
+  } catch (err) {
+    console.error("Could not get all products");
+  }
+}
+
+async function patchProduct(product_id, fields = {}) {
+  const setString = Object.keys(fields)
+    .map((key, index) => `"${key}"=$${index + 1}`)
+    .join(", ");
+  try {
+    if (setString.length > 0) {
+      await client.query(
+        `
+        UPDATE products
+        SET ${setString}
+        WHERE id=${product_id}
+        RETURNING *;
+      `,
+        Object.values(fields)
+      );
+    }
+
+    return await getProductById(product_id);
+  } catch (error) {
+    console.error("Could not patch product ");
+    throw error;
+  }
+}
+
+async function getProductByName(name) {
+  try {
+    const {
+      rows: [product],
+    } = await client.query(
+      `
+      SELECT * FROM products
+      WHERE name = ($1);
+    
+    
+    `,
+      [name]
+    );
+    return product;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function getProductByCategory(category) {
+  try {
+    const { rows } = await client.query(
+      `
+      SELECT *
+      FROM products
+      WHERE type=$1;
+    `,
+      [category]
+    );
+
+    return rows;
+  } catch (error) {
+    console.error("Could not get product by category");
+  }
+}
+
+// USERS FUNCTIONS
+
+const createUser = async ({
+  username,
+  password,
+  email,
+  name,
+  admin,
+  cart = [],
+}) => {
+  try {
+    const {
+      rows: [users],
+    } = await client.query(
+      `
+            INSERT INTO users(
+              username, password, email, name, admin,  cart
+              )
+            VALUES($1, $2, $3, $4, $5, $6)
+            ON CONFLICT (username, email) DO NOTHING
+            RETURNING *;
+         `,
+      [username, password, email, name, admin, cart]
+    );
+
+    return users;
+  } catch (err) {
+    console.error("Could not create users ");
+    throw err;
+  }
+};
+
+async function getAllUsers() {
+  try {
+    const { rows: id } = await client.query(`
+    SELECT id 
+    FROM users;
+  `);
+
+    const users = await Promise.all(id.map((user) => getUserById(user.id)));
+    return users;
+  } catch (err) {
+    throw err;
+  }
+}
+
+async function getUserById(user_id) {
+  try {
+    const {
+      rows: [user],
+    } = await client.query(`
+        SELECT *
+        FROM users
+        WHERE id=${user_id};
+      `);
+
+    if (!user) {
+      throw {
+        name: "UserErrorNotFound",
+        message: "Could not find a user with that user_id",
+      };
+    }
+
+    return user;
+  } catch (err) {
+    console.error("Could not get user by id");
+    throw error;
+  }
+}
+
+async function getUserByUsername(username) {
+  try {
+    const {
+      rows: [user],
+    } = await client.query(
+      `
+    SELECT * FROM users
+    WHERE username = ($1);
+  `,
+      [username]
+    );
+
+    return user;
+  } catch (error) {
+    console.error("Could not grab user by username");
+    throw error;
+  }
+}
+
+async function verifyUniqueUser(username, email, name) {
+  try {
+    const {
+      rows: [user],
+    } = await client.query(
+      `
+    SELECT * FROM users
+    WHERE username = ($1)
+    OR email = ($2)
+    OR name = ($3);
+  `,
+      [username, email, name]
+    );
+
+    return user;
+  } catch (err) {
+    throw err;
+  }
+}
+
+async function patchUser(user_id, fields = {}) {
+  const setString = Object.keys(fields)
+    .map((key, index) => `"${key}"=$${index + 1}`)
+    .join(", ");
+  try {
+    if (setString.length > 0) {
+      await client.query(
+        `
+        UPDATE users
+        SET ${setString}
+        WHERE id=${user_id}
+        RETURNING *;
+      `,
+        Object.values(fields)
+      );
+    }
+
+    return await getUserById(user_id);
+  } catch (error) {
+    console.error("Could not patch product");
+    throw error;
+  }
+}
+
+// USER CART
+
+async function createCartItem(user_id, product_id) {
+  try {
+    await client.query(
+      `
+      INSERT INTO user_cart(user_id, product_id)
+      VALUES ($1, $2)
+      ON CONFLICT (user_id, product_id) DO NOTHING;
+    `,
+      [user_id, product_id]
+    );
+  } catch (error) {
+    console.error("could not create cart item");
+    throw error;
+  }
+}
+
+async function addProductToCart(user_id, product_id) {
+  try {
+    const {
+      rows: [product],
+    } = await client.query(
+      `
+     SELECT *
+     FROM products
+     WHERE id=$1;
+     `,
+      [product_id]
+    );
+    await createCartItem(user_id, product.id);
+
+    return await getUserById(user_id);
+  } catch (error) {
+    console.error("could not add cart item to user");
+    throw error;
+  }
+}
+
+async function getUserById(user_id) {
+  try {
+    const {
+      rows: [user],
+    } = await client.query(
+      `
+      SELECT *
+      FROM users
+      WHERE id=$1;
+    `,
+      [user_id]
+    );
+
+    if (!user) {
+      throw {
+        name: "UserNotFoundError",
+        message: "Could not find a User with that user_id",
+      };
+    }
+
+    const { rows: products } = await client.query(
+      `
+      SELECT products.*
+      FROM products
+      JOIN user_cart ON products.id=user_cart.product_id
+      WHERE user_cart.user_id=$1;
+    `,
+      [user_id]
+    );
+
+    user.cart = products;
+
+    return user;
+  } catch (error) {
+    throw error;
+  }
+}
+
 module.exports = {
   client,
-  // db methods
+  createProduct,
+  getAllProducts,
+  getProductById,
+  getProductByCategory,
+  patchProduct,
+  createUser,
+  getAllUsers,
+  getProductByName,
+  getUserById,
+  getUserByUsername,
+  verifyUniqueUser,
+  patchUser,
+  addProductToCart,
 };
--- my-app/db/init_db.js
+++ my-app/db/init_db.js
@@ -1,30 +1,201 @@
-// code to build and initialize DB goes here
 const {
   client,
-  // other db methods
+  createProduct,
+  getAllProducts,
+  getProductById,
+  getProductByCategory,
+  patchProduct,
+  createUser,
+  getAllUsers,
+  patchUser,
+  addProductToCart,
 } = require("./index");
+``;
 
 async function buildTables() {
   try {
-    client.connect();
+    client.query(`
+        DROP TABLE IF EXISTS user_cart;
+        DROP TABLE IF EXISTS users;
+        DROP TABLE IF EXISTS products;
+      `);
 
-    // drop tables in correct order
+    console.log("Starting to build tables...");
 
-    // build tables in correct order
+    await client.query(`
+      CREATE TABLE products(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255) UNIQUE NOT NULL,
+        date_created DATE NOT NULL DEFAULT CURRENT_DATE,
+        description VARCHAR(255) NOT NULL,
+        price DECIMAL DEFAULT 0,
+        image_url TEXT NOT NULL,
+        category VARCHAR(255) NOT NULL,
+        quantity INTEGER,
+        inventory INTEGER DEFAULT 0
+        
+        
+      );
+       CREATE TABLE users(
+        id SERIAL PRIMARY KEY,
+        username VARCHAR(255) UNIQUE NOT NULL,
+        password VARCHAR(255) NOT NULL,
+        email VARCHAR(255) UNIQUE NOT NULL,
+        name VARCHAR(255) NOT NULL,
+        admin BOOLEAN DEFAULT FALSE,
+        cart TEXT [],
+        UNIQUE(username, email)
+      );
+      CREATE TABLE user_cart(
+        user_id INTEGER REFERENCES users(id),
+        product_id INTEGER REFERENCES products(id),
+        UNIQUE(user_id, product_id)
+      );
+      `);
+    console.log("Finished building tables...");
   } catch (error) {
     throw error;
   }
 }
 
-async function populateInitialData() {
+const createInitialProducts = async () => {
+  console.log("Starting to create initial products...");
   try {
-    // create useful starting data
+    const productsToCreate = [
+      {
+        name: "product 1",
+        date_created: "2020/08/31",
+        description: "Link One Description.",
+        price: 1.25,
+        image_url: "https://picsum.photos/id/217/200/300",
+        category: "vodka",
+        quantity: 2,
+        inventory: "5",
+      },
+      {
+        name: "product 2",
+        date_created: "2021/12/11",
+        description: "Link Two Description.",
+        price: 16.99,
+        image_url: "https://picsum.photos/id/227/200/300",
+        category: "gin",
+        quantity: 1,
+        inventory: "6",
+      },
+      {
+        name: "product 3",
+        date_created: "2019/02/07",
+        description: "Link three Description.",
+        price: 47.99,
+        image_url: "https://picsum.photos/id/237/200/300",
+        category: "whiskey",
+        quantity: 1,
+        inventory: "7",
+      },
+    ];
+    const products = await Promise.all(productsToCreate.map(createProduct));
+    console.log("Products created:");
+    console.log(products);
+    console.log("Finished creating products!");
+  } catch (err) {
+    console.error("There was a problem creating PRODUCTS");
+    throw err;
+  }
+};
+
+const createInitialUsers = async () => {
+  console.log("Starting to create initial users...");
+  try {
+    const usersToCreate = [
+      {
+        username: "test",
+        password: "test123",
+        email: "hello@gmail.com",
+        name: "Danielle Scott",
+        admin: "true",
+      },
+      {
+        username: "Ali123",
+        password: "hellothere",
+        email: "ali123@gmail.com",
+        name: "Ali Miles",
+        admin: "false",
+      },
+      {
+        username: "testing123",
+        password: "testing567",
+        email: "testing123@gmail.com",
+        name: "test",
+        admin: "false",
+      },
+    ];
+    const users = await Promise.all(usersToCreate.map(createUser));
+    console.log("Users created:");
+    console.log(users);
+    console.log("Finished creating users!");
+  } catch (err) {
+    console.error("There was a problem creating USERS");
+    throw err;
+  }
+};
+
+async function rebuildDB() {
+  try {
+    client.connect();
+    await buildTables();
+    await createInitialProducts();
+    await createInitialUsers();
   } catch (error) {
     throw error;
   }
 }
 
-buildTables()
-  .then(populateInitialData)
+async function testDB() {
+  try {
+    console.log("Starting to test database...");
+
+    console.log("Calling getAllProducts");
+    const products = await getAllProducts();
+    console.log("Result:", products);
+
+    console.log("Calling getAllUsers");
+    const users = await getAllUsers();
+    console.log("Result:", users);
+
+    console.log("Calling getProductByCategory");
+    const productByCategory = await getProductByCategory("whiskey");
+    console.log("Result:", productByCategory);
+
+    console.log("Calling addProductToCart");
+    const userWithProduct = await addProductToCart(1, 2);
+    console.log("Result:", userWithProduct);
+
+    console.log("Calling addProductToCart");
+    const userWithSecondProduct = await addProductToCart(1, 1);
+    console.log("Result:", userWithSecondProduct);
+
+    console.log("Calling patchProduct");
+    const updatedProduct = await patchProduct(1, {
+      name: "updated product",
+      inventory: 20,
+    });
+    console.log("Result:", updatedProduct);
+
+    console.log("Calling patchUser");
+    const updatedUser = await patchUser(1, {
+      name: "Jenny Roy",
+      username: "coder567",
+    });
+    console.log("Result:", updatedUser);
+
+    console.log("Finished database tests!");
+  } catch (error) {
+    console.log("Error during testDB");
+    throw error;
+  }
+}
+
+rebuildDB()
+  .then(testDB)
   .catch(console.error)
   .finally(() => client.end());
