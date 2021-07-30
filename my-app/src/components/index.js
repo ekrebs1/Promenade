b439d0c29ad0a05ad1632643533eef88dc01e31a
Erica <ericakrebs1@gmail.com>
2021-07-30 14:41:34 -0500

    Clean Code

--- my-app/src/components/index.js
+++
@@ -1,8 +0,0 @@
-export { default as Navbar } from "./Navbar/Navbar";
-export { default as ProductsList } from "./Products/ProductsList";
-export { default as Login } from "./Login";
-export { default as Home } from "./Home";
-export { default as CreateForm } from "./CreateForm/CreateForm";
-export { default as Register } from "./Register";
-export { default as Cart } from "./Cart/Cart";
-// export { default as Users } from "./Users/Users";
