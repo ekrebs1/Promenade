08167a328914306980c08571a3e8e3e20c74de1b
Erica <ericakrebs1@gmail.com>
2021-08-03 18:23:11 -0500

    created api helper function methods

---
+++ my-app/src/api/utils.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+export const clearToken = () => {
+  localStorage.removeItem("token");
+};
+
+export const setToken = (token) => {
+  localStorage.setItem("token", token);
+};
+
+export const getToken = () => {
+  return localStorage.getItem("token");
+};
+
+export const loggedAdmin = () => {
+  localStorage.setItem("admin", "isAdmin");
+};
+
+export const clearAdmin = () => {
+  localStorage.removeItem("admin");
+};
