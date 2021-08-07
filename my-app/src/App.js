05f34831c18142c84d43033077cb03ede6e37609
Erica <ericakrebs1@gmail.com>
2021-08-07 16:53:33 -0500

    react routing

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import CategoryButtons from "./components/Home/CategoryButtons";
 
-function App() {
-  return <div className='App'></div>;
-}
+const App = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  useEffect(() => {
+    if (localStorage.getItem("token")) {
+      setLoggedIn(true);
+    }
+  }, [setLoggedIn]);
+
+  return (
+    <Router>
+      <Navbar />
+      <main>
+        <Switch>
+          <Route exact path='/'>
+            <CategoryButtons loggedIn={loggedIn} />
+          </Route>
+        </Switch>
+      </main>
+    </Router>
+  );
+};
 
 export default App;
