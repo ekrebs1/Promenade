3c95b7a10ca86d9884a32e408f68d76238b517eb
Erica <ericakrebs1@gmail.com>
2021-08-25 22:19:15 -0500

    make it better

---
+++ src/App.js
@@ -0,0 +1,82 @@
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  Home,
+  Navbar,
+  Register,
+  Login,
+  Products,
+  Cart,
+  Coverups,
+} from "./components/index";
+
+const App = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [cart, setCart] = useState([]);
+  const [setAdmin] = useState(false);
+  const [user, setUser] = useState("");
+
+  useEffect(() => {
+    if (localStorage.getItem("Cart")) {
+      setCart(JSON.parse(localStorage.getItem("Cart")));
+    }
+  }, []);
+
+  useEffect(() => {
+    if (localStorage.getItem("token")) {
+      setLoggedIn(true);
+    }
+  }, [setLoggedIn]);
+
+  useEffect(() => {
+    if (localStorage.getItem("admin")) {
+      setAdmin(true);
+    }
+  });
+
+  useEffect(() => {
+    if (localStorage.getItem("user")) {
+      setUser(localStorage.getItem("user"));
+    }
+  }, [user]);
+
+  useEffect(() => {
+    localStorage.setItem("Cart", JSON.stringify(cart));
+  }, [cart]);
+
+  return (
+    <Router>
+      <Navbar />
+      <main>
+        <Switch>
+          <Route exact path='/'>
+            <Home loggedIn={loggedIn} />
+          </Route>
+          <Route exact path='/register'>
+            <Register loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
+          </Route>
+          <Route exact path='/login'>
+            <Login
+              loggedIn={loggedIn}
+              setLoggedIn={setLoggedIn}
+              setAdmin={setAdmin}
+              user={user}
+              setUser={setUser}
+            />
+          </Route>
+          <Route exact path='/products'>
+            <Products cart={cart} setCart={setCart} loggedIn={loggedIn} />
+          </Route>
+          <Route exact path='/cart'>
+            <Cart cart={cart} setCart={setCart} loggedIn={loggedIn} />
+          </Route>
+          <Route exact path='/cover-ups'>
+            <Coverups cart={cart} />
+          </Route>
+        </Switch>
+      </main>
+    </Router>
+  );
+};
+
+export default App;
